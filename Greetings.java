e9c4ee0b51820f38034e830195686f60a35837b9
Okello Desmond <okellodesmond@gmail.com>
2019-10-20 18:20:08 +0800

    edited java file

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,8 @@
 public class Greetings{
     public static void main(String []args){
-        System.out.println("Hello Jenkins");
+        for(int x=0; x<10; x++){
+   System.out.println(x+": Hello Jenkins");
+        }
+     
     }
 }
